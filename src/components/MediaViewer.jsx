1d4406979616471495012b461f59f65c70b630a7
Mukesh-Thidiff <mukesh.murugaiyan@gmail.com>
2025-04-15 17:40:50 +0530

    added layzy loading

--- src/components/MediaViewer.jsx
+++ src/components/MediaViewer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Modal } from 'reactstrap';
 import { MdClose } from 'react-icons/md';
 import { FaYoutube } from 'react-icons/fa';
@@ -6,6 +6,7 @@ import ReactPlayer from 'react-player';
 import Lightbox from 'yet-another-react-lightbox';
 import 'yet-another-react-lightbox/styles.css';
 import ArrayList from '../lib/ArrayList';
+import useIntersectionObserver from '../lib/Media';
 
 export const isVideoURL = (url) => {
   const videoExtensions = ['.mp4', '.avi', '.mov', '.mkv'];
@@ -16,30 +17,36 @@ export const isVideoURL = (url) => {
 const MediaViewer = ({ media_url }) => {
   const [openMedia, setOpenMedia] = useState(null);
 
-  const mediaList = ArrayList.isArray(media_url) ? media_url : media_url ? media_url.split(","):[];
+  const mediaList = ArrayList.isArray(media_url) ? media_url : media_url ? media_url.split(",") : [];
 
   return (
     <div className="media-gallery d-flex gap-3 flex-wrap">
-      {mediaList.map((url, index) => (
-        <div key={index} className="media-item">
-          {isVideoURL(url) ? (
-            <FaYoutube
-              size={90}
-              color="black"
-              onClick={() => setOpenMedia({ type: 'video', url })}
-              style={{ cursor: 'pointer' }}
-            />
-          ) : (
-            <img
-              src={url}
-              alt="media"
-              className="message-image"
-              onClick={() => setOpenMedia({ type: 'image', url })}
-              style={{ width: 100, height: 100, objectFit: 'cover', cursor: 'pointer' }}
-            />
-          )}
-        </div>
-      ))}
+      {mediaList.map((url, index) => {
+        const imgRef = useRef(null);
+        const isVisible = useIntersectionObserver(imgRef, true); // you can disable for modal too
+
+        return (
+          <div key={index} className="media-item">
+            {isVideoURL(url) ? (
+              <FaYoutube
+                size={90}
+                color="black"
+                onClick={() => setOpenMedia({ type: 'video', url })}
+                style={{ cursor: 'pointer' }}
+              />
+            ) : (
+              <img
+                ref={imgRef}
+                src={isVisible ? url : ''}
+                alt="media"
+                className="message-image"
+                onClick={() => setOpenMedia({ type: 'image', url })}
+                style={{ width: 100, height: 100, objectFit: 'cover', cursor: 'pointer', background: '#e0e0e0' }}
+              />
+            )}
+          </div>
+        );
+      })}
 
       {openMedia?.type === 'video' && (
         <Modal isOpen={true} toggle={() => setOpenMedia(null)}>
